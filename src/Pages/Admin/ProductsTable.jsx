5a823831f818faf9c7e246b47c0f0712bdb80dda
maharrambayramov <meherrem.bayramov.03@gmail.com>
2023-05-18 11:06:06 +0400

    products table created

--- src/Pages/Admin/ProductsTable.jsx
+++ src/Pages/Admin/ProductsTable.jsx
@@ -1,54 +1,54 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 import {
-    Table,
-    Thead,
-    Tbody,
-    Tr,
-    Th,
-    Td,
-    TableCaption,
-    TableContainer,
-} from '@chakra-ui/react'
-import axios from 'axios'
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
+} from "@chakra-ui/react";
+import axios from "axios";
 
 const ProductsTable = () => {
-    const [products, setProducts] = useState([])
+  const [products, setProducts] = useState([]);
 
-    useEffect(() => {
-        const getData = async () => {
-            const res =await axios.get("https://northwind.vercel.app/api/categories");
-            setProducts(res.data)
-        }
-        getData()
-    })
+  useEffect(() => {
+    const getData = async () => {
+      const res = await axios.get(
+        "http://localhost:5050/users"
+      );
+      setProducts(res.data);
+    };
+    getData();
+  });
 
-    const productsUpdate =
-        products.sort((a,b) => (a.id-b.id))
-    return (
-        <TableContainer>
-            <Table variant='simple'>
-                <TableCaption>Imperial to metric conversion factors</TableCaption>
-                <Thead>
-                    <Tr>
-                        <Th>ID</Th>
-                        <Th>Name</Th>
-                        <Th>Unit Price</Th>
-                    </Tr>
-                </Thead>
-                <Tbody>
-                    {productsUpdate&&productsUpdate.map(product => (
-                        <Tr>
-                            <Td>{product.id}</Td>
-                            <Td>{product.name}</Td>
-                            <Td >{product.unitPrice}</Td>
-                        </Tr>
-                    ))}
+  const productsUpdate = products.sort((a, b) => a.id - b.id);
+  return (
+    <TableContainer>
+      <Table variant="simple">
+        <TableCaption>Imperial to metric conversion factors</TableCaption>
+        <Thead>
+          <Tr>
+            <Th>ID</Th>
+            <Th>Name</Th>
+            <Th>Unit Price</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {productsUpdate &&
+            productsUpdate.map((product) => (
+              <Tr>
+                <Td>{product._id}</Td>
+                <Td>{product.username}</Td>
+                <Td>{product.password}</Td>
+              </Tr>
+            ))}
+        </Tbody>
+      </Table>
+    </TableContainer>
+  );
+};
 
-                </Tbody>
-
-            </Table>
-        </TableContainer>
-    )
-}
-
-export default ProductsTable
+export default ProductsTable;
--- src/Pages/Client/Products.jsx
+++ src/Pages/Client/Products.jsx
@@ -3,6 +3,17 @@ import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { index } from "../../Schema";
 import axios from "axios";
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
+} from "@chakra-ui/react";
 
 const Products = () => {
   const [products1, setProducts1] = useState([]);
@@ -14,14 +25,14 @@ const Products = () => {
   const postData = async () => {
     const values = getValues();
     console.log(values);
-    await axios.post("https://northwind.vercel.app/api/categories", {
+    await axios.post("http://localhost:5050/users", {
       name: values.name,
       price: values.unitPrice,
     });
   };
   useEffect(() => {
     const getData = async () => {
-      const res = await axios.get("https://northwind.vercel.app/api/categories");
+      const res = await axios.get("http://localhost:5050/users");
       setProducts1(res.data);
     };
     getData();
@@ -42,10 +53,24 @@ const Products = () => {
       </form>
       {products1 &&
         products1.map((d) => (
-          <>
-            <h1>{d.name}</h1>
-            <p>{d.unitPrice}</p>
-          </>
+          <TableContainer>
+            <Table size="sm">
+              <Thead>
+                <Tr>
+                  <Th>Name</Th>
+                  <Th>Id</Th>
+                  <Th>Password</Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                <Tr>
+                  <Td>{d.username}</Td>
+                  <Td>{d._id}</Td>
+                  <Td>{d.password}</Td>
+                </Tr>
+              </Tbody>
+            </Table>
+          </TableContainer>
         ))}
     </div>
   );
